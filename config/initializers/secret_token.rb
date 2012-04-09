b156585ee88893ff1bef0f9776562a65f7953290
U-AP\405781 <405781@TEP-U405781L002.AP.ad.seagate.com>
2012-04-09 12:11:40 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'b64a0de7e6307a086fed4001e70e3d42f2afc7068db2aab546cdb57f7360b3805bb8c5d86ccc1cf08d4ec20f0cb9818a876460b3818b7e2d3038afb786e667e9'
